3859f83467768446631e7ff44dbb325eb4b0feef
mohamabdu936-rgb <mohamabdu936@gmail.com>
2026-06-17 15:24:48 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"log"
 	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
